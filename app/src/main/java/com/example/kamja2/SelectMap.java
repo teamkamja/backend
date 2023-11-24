9714c9d339005e139e9eca22340b10ea88a83241
april <sue5520607@skuniv.ac.kr>
2023-11-24 13:17:25 +0900

    메인페이지에 지도 또 수정

--- app/src/main/java/com/example/kamja2/SelectMap.java
+++ app/src/main/java/com/example/kamja2/SelectMap.java
@@ -1,11 +1,25 @@
 package com.example.kamja2;
 
+import static androidx.core.location.LocationManagerCompat.getCurrentLocation;
+
 import androidx.fragment.app.FragmentActivity;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -14,11 +28,21 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.kamja2.databinding.ActivitySelectMapBinding;
+
+
+
+
 // 지도 페이지
 public class SelectMap extends FragmentActivity implements OnMapReadyCallback {
+    //구글 맵 사용
 
     private GoogleMap mMap;
+    //xml 레이아웃 view 요소에 접근하고 조작할 수 있게 하는거
     private ActivitySelectMapBinding binding;
+
+
+    //안드로이드 디바이스 현재위치 얻기 위해 사용
+    private FusedLocationProviderClient fusedLocationProviderClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +56,9 @@ public class SelectMap extends FragmentActivity implements OnMapReadyCallback {
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+//임시로 만들어둔 홈버튼
         Button home = (Button) findViewById(R.id.home);
 
         home.setOnClickListener(new View.OnClickListener() {
@@ -41,9 +68,44 @@ public class SelectMap extends FragmentActivity implements OnMapReadyCallback {
                 startActivity(intent);
             }
         });
+
+
+       //위치 요청 권한 보내기 위해서
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                // Permission already granted
+                getCurrentLocation();
+            } else {
+                // Request location permission
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+        } else {
+            // For devices below Marshmallow, permission is granted at installation time
+            getCurrentLocation();
+        }
     }
 
+//현재 위치 가져오기
+    //오류 나는거 아니고 권한이 거부될 수 있다고 말하는거임!!
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
 
+            fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful() && task.getResult() != null) {
+                Location location = task.getResult();
+                LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+                mMap.addMarker(new MarkerOptions().position(currentLatLng).title("My Location"));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLatLng));
+                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 18f)); // 15f는 줌 레벨, 조절 가능
+
+            }
+        });
+    }
+        else {
+            // 권한이 없는 경우 권한 요청
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -58,8 +120,17 @@ public class SelectMap extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+       // LatLng sydney = new LatLng(-34, 151);
+        //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        getCurrentLocation();
+
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == 1 && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            getCurrentLocation();
+        }
     }
 }
